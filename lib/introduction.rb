b64d0fbe2b83d64e51902cd98759a163d57ca906
Corina Talavera <corina.talavera@gmail.com>
2018-10-17 23:26:42 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 def introduction (first_name, last_name)
-  puts "Hi, my full name is #{first_name,last_name}."
+  puts "Hi, my full name is #{first_name} #{last_name}."
 end
 
 introduction("Bob", "Brown")
