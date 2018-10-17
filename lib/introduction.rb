7fe0f98f8e0b88cbc3f1569fb43054823e732a11
Corina Talavera <corina.talavera@gmail.com>
2018-10-17 23:32:02 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,5 @@
-def introduction (first_name, last_name)
+def introduction(first_name, last_name)
+  
   puts "Hi, my full name is #{first_name} #{last_name}."
 end
 
@@ -6,5 +7,4 @@ introduction("Bob", "Brown")
 # Hi my full name is Bob Brown
 introduction("Bob", "Jackson")
 # I am not part of the Brown family
-puts "I am not part of the Brown family"(first_name,last_name2)
-last_name2=Brown
+instroduction()
