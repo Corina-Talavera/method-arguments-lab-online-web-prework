07adbccfce10f2ed48bb30fbeeac529b3a39db3c
Corina Talavera <corina.talavera@gmail.com>
2018-10-17 23:30:22 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,3 +6,5 @@ introduction("Bob", "Brown")
 # Hi my full name is Bob Brown
 introduction("Bob", "Jackson")
 # I am not part of the Brown family
+puts "I am not part of the Brown family"(first_name,last_name2)
+last_name2=Brown
