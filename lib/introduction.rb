2e2ed41b2879ef53ddbeb7675d6ef5e4d6fc1451
Corina Talavera <corina.talavera@gmail.com>
2018-10-17 23:33:57 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,7 @@ def introduction(first_name, last_name)
   if last_name != "Brown" 
     puts " I am not part of the Brown family"
   else
-    puts "Hi, my full name is #{first_name} #{last_name}."
+    puts "Hi, my full name is #{first_name} #{last_name} I am part of the Brown family."
   end
   
 end
